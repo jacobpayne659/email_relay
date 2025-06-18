366f4bda3dac007f2384111fc4995e3194338ea5
Jacob Payne <paynejacob92@gmail.com>
2025-06-18 16:51:05 -0400

    add name to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   has_many :email_groups, through: :email_group_memberships
 
   validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
 end
