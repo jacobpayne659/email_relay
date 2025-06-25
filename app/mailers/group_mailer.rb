569dbacb8d60257559e0f2a92426bb530cd98a39
Jacob Payne <paynejacob92@gmail.com>
2025-06-25 15:32:45 -0400

    Update group_mailer.rb

--- app/mailers/group_mailer.rb
+++ app/mailers/group_mailer.rb
@@ -2,7 +2,7 @@ class GroupMailer < ApplicationMailer
   def forward_email(user, original_mail)
     mail(
       to: user.email,
-      from: original_mail.from&.first || "default@example.com",
+      from: original_mail.from&.first || "default@email.com",
       subject: original_mail.subject,
       body: original_mail.body.decoded
     )
